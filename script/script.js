e1bb6ac17c72cb9f193fe9a8ea0069cdf5f76eee
bsgabriel <gabrielbs22@icloud.com>
2022-10-24 22:24:08 -0300

    Criação do grid de pré requisitos

--- script/script.js
+++ script/script.js
@@ -1,35 +1,42 @@
 window.addEventListener('load', function () {
   loadHeader();
 
-  let table = document.getElementById('tabelaCursos');
+  const table = document.getElementById('tabelaCursos');
   loadTable(table);
   addRowHandlers(table);
 });
 
+/**
+ * Carrega as informações do curso, no cabeçalho
+ */
 function loadHeader() {
   document.getElementById('nomeCurso').textContent = jsonData.CURSO;
   document.getElementById('codCurso').textContent = jsonData.CODIGO_DO_CURSO;
 
-  let duracao = document.getElementById("infoDuracao");
+  const duracao = document.getElementById("infoDuracao");
   const txtDuracao = 'Duração: ' + jsonData.DURACAO;
   const txtCargaHoraria = 'Carga Horária: ' + jsonData.CARGA_HORARIA;
   duracao.innerHTML = txtDuracao + '<br>' + txtCargaHoraria;
 }
 
+/**
+ * Carrega os dados em uma tabela
+ * @param {Object} table 
+ */
 function loadTable(table) {
   for (let j = 0; j < jsonData.DISCIPLINAS.length; j++) {
     // let row = table.insertRow(j + 1);
-    let row = table.getElementsByTagName('tbody')[0].insertRow(table.length);
+    const row = table.getElementsByTagName('tbody')[0].insertRow(table.length);
     row.classList.add('content');
     row.setAttribute('data-toggle', 'modal');
     row.setAttribute('data-target', '#janelaModal');
     row.setAttribute('data-id', j);
 
-    let idx = row.insertCell(0);
-    let semestre = row.insertCell(1);
-    let codDisciplina = row.insertCell(2);
-    let nomeDisciplina = row.insertCell(3);
-    let cargaHoraria = row.insertCell(4);
+    const idx = row.insertCell(0);
+    const semestre = row.insertCell(1);
+    const codDisciplina = row.insertCell(2);
+    const nomeDisciplina = row.insertCell(3);
+    const cargaHoraria = row.insertCell(4);
 
     idx.innerHTML = j;
     semestre.innerHTML = jsonData.DISCIPLINAS[j].SEMESTRE;
@@ -49,11 +56,15 @@ function loadTable(table) {
   }
 }
 
+/**
+ * Adiciona eventos de clique nas linhas de uma tabela
+ * @param {Object} table 
+ */
 function addRowHandlers(table) {
   Array.from(table.rows).forEach(row => {
     row.onclick = function () {
       return function () {
-        let id = this.cells[0].innerHTML;
+        const id = this.cells[0].innerHTML;
         const codigo = document.getElementById('modalCodDisciplina');
         const txtCodigo = jsonData.DISCIPLINAS[id].CODIGO;
         codigo.innerHTML = txtCodigo;
@@ -84,7 +95,51 @@ function addRowHandlers(table) {
 
         outros.innerHTML = txtSemestre + ' - ' + txtModalidade + ' - ' + txtDuracao;
 
+        const preRequisitos = jsonData.DISCIPLINAS[id].PREREQUISITOS;
+
+        if (typeof preRequisitos !== 'undefined') {
+          createGrid(preRequisitos);
+        }
       };
     }(row);
   });
+}
+
+
+/**
+ * Chama a função de criação de itens da lista com base no tipo de dado que recebe. 
+ * Pode receber um array de string ("1, 2, 3..") ou um valor numérico.
+ * @param {string} requisitos 
+ */
+function createGrid(requisitos) {
+  if (typeof requisitos === 'string') {
+    requisitos = requisitos.replace(' ', '');
+    arrRequisitos = requisitos.split(',').map(Number);
+    arrRequisitos.forEach(req => {
+      createGridItem(req);
+    });
+  } else if (typeof requisitos === 'number') {
+    createGridItem(requisitos);
+  }
+};
+
+/**
+ * Cria uma div com as informações da disciplina que possui a ordem informada
+ * @param {number} requisito 
+ */
+function createGridItem(ordem) {
+  const grid = document.getElementById('gridPreRequisitos');
+  const item = document.createElement('div');
+
+  const codDisciplina = document.createElement('p');
+  codDisciplina.textContent = jsonData.DISCIPLINAS[ordem].CODIGO;
+
+  const nomeDisciplina = document.createElement('p');
+  nomeDisciplina.textContent = jsonData.DISCIPLINAS[ordem].DISCIPLINA;
+
+  // item.classList.add('col');
+  item.classList.add('grid-item');
+  item.appendChild(codDisciplina);
+  item.appendChild(nomeDisciplina);
+  grid.appendChild(item);
 }
